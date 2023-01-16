358f134eda369db643494aa3595e00c9e0aeb8ec
pizone2 <rlawlstj0123@naver.com>
2023-01-16 17:45:48 +0900

    commit

---
+++ src/com/iu/api5/thred/ThreadTest1.java
@@ -0,0 +1,29 @@
+package com.iu.api5.thred;
+
+public class ThreadTest1 extends Thread {
+
+	@Override
+	public void run() {
+		this.t1();
+	}
+	
+	public void t1() {
+		
+			for(int i=0; i < 26; i++) {
+				System.out.println("T1 : "+i);
+				try {
+					Thread.sleep(1000);
+				} catch (InterruptedException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+			}
+	}
+
+
+
+
+
+
+
+}
---
+++ src/com/iu/api5/thred/ThreadTest3.java
@@ -0,0 +1,21 @@
+package com.iu.api5.thred;
+
+public class ThreadTest3 implements Runnable {
+	
+	@Override
+	public void run() {
+		this.t3();
+	}
+	
+	private void t3() {
+		for(int i=0; i<25 ;i++) {
+			System.out.println("Hello : "+i);
+			try {
+				Thread.sleep(500);
+			} catch (InterruptedException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+	}
+}
